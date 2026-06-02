df7b0deba97c8cbe1cac8e113903e1f9d4c5a93d
Rwanbt <barat.erwan@gmail.com>
2026-06-02 14:31:51 +0200

    Fix: mistake syntax

--- sql_join/3-students-and-courses.sql
+++ sql_join/3-students-and-courses.sql
@@ -1,5 +1,5 @@
 SELECT students.name AS student_name, courses.title AS course_title,
 FROM students
-INNER JOIN enrollments ON student.id = enrollements.student_id
+INNER JOIN enrollments ON student.id = enrollments.student_id
 INNER JOIN courses ON enrollments.course_id = course.id
 ORDER BY student_name ASC, course_title ASC;
