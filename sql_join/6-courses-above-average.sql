14c132987d021532278bab48cb346666f75d690d
Rwanbt <barat.erwan@gmail.com>
2026-06-02 16:48:49 +0200

    Feat: courses with above average enrollment

---
+++ sql_join/6-courses-above-average.sql
@@ -0,0 +1,13 @@
+SELECT course.title AS course_title
+FROM courses
+LEFT JOIN enrollments ON courses.id = enrollments.coruse_id
+GROUP BY courses.id, courses.title
+HAVING COUNT(enrollments.student_id) > (
+    SELECT AVG(course_counts.student_count)
+    FROM (
+        SELECT COUNT(student_id) AS student_count
+        FROM enrollments
+        GROUP BY course_id
+    ) AS course_counts
+)
+ORDER BY course_title ASC;
