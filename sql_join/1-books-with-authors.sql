a43a1de9c287c2cd98c68b22e4ab064bdc144db9
Rwanbt <barat.erwan@gmail.com>
2026-06-02 11:14:28 +0200

    Feat: books with authors mistake id

--- sql_join/1-books-with-authors.sql
+++ sql_join/1-books-with-authors.sql
@@ -1,4 +1,4 @@
 SELECT books.title AS title, authors.name AS author_name
 FROM books
-INNER JOIN authors ON books.author_id = authors.INDEX
+INNER JOIN authors ON books.author_id = authors.id
 ORDER BY title ASC
